3c8ae6a8bb26940c6786c09e1e76aa4ae7dcdaa0
hbajwaa <bajwahassan3@outlook.com>
2020-08-11 21:45:34 -0400

    created home page

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="Home-Page">Welcome to Pet Land!</h1>
+      </div>
+    )
+  }
+}
+
+export default Home
