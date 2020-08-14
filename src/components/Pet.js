2e595ad7d81d5b583f283ba8527055760dc27212
hbajwaa <bajwahassan3@outlook.com>
2020-08-14 04:35:26 -0400

    presentational pet component

---
+++ src/components/Pet.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {deletePet} from '../actions/deletePet'
+import { connect } from 'react-redux'
+
+const Pet = ({pet, deletePet}) => {
+    return (
+    <li className="pet">
+      Pet Name: {pet.name}<br />
+      Age: { pet.age } <br />
+      Species: { pet.species } <br />
+      Breed: { pet.breed } <br />
+      Description: { pet.description } <br />
+      <button onClick={()=>deletePet(pet.id)}>delete</button> <br/>
+      -----------------------------------
+    </li>
+    );
+}
+
+export default connect(null, { deletePet })(Pet)
+// export default Pet
