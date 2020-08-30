a7b6ed9a632a91df0910a723c572c5ab3eb5f5c1
hbajwaa <bajwahassan3@outlook.com>
2020-08-30 02:06:47 -0400

    updatedà

--- src/reducers/petsReducer.js
+++ src/reducers/petsReducer.js
@@ -1,13 +1,17 @@
 export default (state=[], action) => {
 
   switch(action.type){
+    
     case "SET_PETS":
       return [...action.pets]
+
     case "ADD_PET":
       return [...state, action.pet]
+
     case "DELETE_PET":
       let newPets = state.filter(pet => pet.id !== action.petId)
       return [...newPets]
+
     default:
       return state
   }
